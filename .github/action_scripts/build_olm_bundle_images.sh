15aa73fed00f3ccacbd4bfadb3d82206bd67d169
Oleksandr Andriienko <oandriie@redhat.com>
2020-10-16 17:56:12 +0300

    Fix up.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- .github/action_scripts/build_olm_bundle_images.sh
+++ .github/action_scripts/build_olm_bundle_images.sh
@@ -43,7 +43,7 @@ do
   export incrementPart=$(getNightlyVersionIncrementPart "${nightlyVersion}")
   echo "[INFO] Nightly increment version ${incrementPart}"
 
-  export CHECK_NIGHTLY_TAG=$("skopeo inspect docker://${IMAGE_REGISTRY}/${IMAGE_REGISTRY_USERNAME}/eclipse-che-kubernetes-opm-bundles:${nightlyVersion} 2>/dev/null | jq -r '.RepoTags[]|select(. == \"${nightlyVersion}\")'")
+  export CHECK_NIGHTLY_TAG=$(skopeo inspect docker://quay.io/aandriienko/eclipse-che-kubernetes-opm-bundles:${nightlyVersion} 2>/dev/null | jq -r '.RepoTags[]|select(. == "${nightlyVersion}")')
   echo "=======================================${CHECK_NIGHTLY_TAG}=========================="
   if [ -z "$CHECK_NIGHTLY_TAG" ]
   then
